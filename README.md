a97e22400d52332770667d15319650ea15d9a07b
Prabhat Shukla <prabhat47.1928@gmail.com>
2017-09-30 15:54:57 +0530

    First commit Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Social-Media-Web-app
---
+++ simplesocial/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+class TestPage(TemplateView):
+    template_name = 'test.html'
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+class HomePage(TemplateView):
+    template_name = 'index.html'
