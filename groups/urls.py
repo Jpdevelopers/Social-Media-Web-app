e8d67bfb9aa2cdcf033a6f060aac5e6421f299c8
Kunal Singhal <ksinghal1997@gmail.com>
2017-10-02 18:22:56 +0530

    Add files via upload

--- groups/urls.py
+++ groups/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     url(r"^new/$", views.CreateGroup.as_view(), name="create"),
     url(r"^posts/in/(?P<slug>[-\w]+)/$",views.SingleGroup.as_view(),name="single"),
     url(r"join/(?P<slug>[-\w]+)/$",views.JoinGroup.as_view(),name="join"),
-    # url(r"leave/(?P<slug>[-\w]+)/$",views.LeaveGroup.as_view(),name="leave"),
 ]
--- groups/views.py
+++ groups/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
 from django.contrib import messages
-# Create your views here.
 from django.contrib.auth.mixins import(
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
-from . import models
+
 from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 from django.views import generic
 from groups.models import Group,GroupMember
-from django.shortcuts import get_object_or_404
+from . import models
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ("name", "description")
@@ -21,15 +21,22 @@ class SingleGroup(generic.DetailView):
 class ListGroups(generic.ListView):
     model = Group
 
+
 class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
     def get(self, request, *args, **kwargs):
-        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+        group = get_object_or_404(Group,slug=self.kwargs.get("slug"))
+
         try:
-            GroupMember.objects.Create(user=self.request.user, group=group)
+            GroupMember.objects.create(user=self.request.user,group=group)
+
         except IntegrityError:
-            messages.warning(self.request, 'Warning already a member!')
+            messages.warning(self.request,("Warning, already a member of {}".format(group.name)))
+
         else:
-            messages.success(self.request, 'You are now a member!')
-        return super().get(request,*args,**kwargs)
+            messages.success(self.request,"You are now a member of the {} group.".format(group.name))
+
+        return super().get(request, *args, **kwargs)
