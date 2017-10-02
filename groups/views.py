5ebdcfdc4d910f214a90b33d5788c2b893143f9f
Kunal Singhal <ksinghal1997@gmail.com>
2017-10-02 18:38:30 +0530

    Added a feature to leave group

--- groups/views.py
+++ groups/views.py
@@ -40,3 +40,31 @@ class JoinGroup(LoginRequiredMixin, generic.RedirectView):
             messages.success(self.request,"You are now a member of the {} group.".format(group.name))
 
         return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("groups:single",kwargs={"slug": self.kwargs.get("slug")})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get("slug")
+            ).get()
+
+        except models.GroupMember.DoesNotExist:
+            messages.warning(
+                self.request,
+                "You can't leave this group because you aren't in it."
+            )
+        else:
+            membership.delete()
+            messages.success(
+                self.request,
+                "You have successfully left this group."
+            )
+        return super().get(request, *args, **kwargs)
