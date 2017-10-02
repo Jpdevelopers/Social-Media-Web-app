076367d773ea64472c1d4e4906ff12a46fd55ae8
Kunal Singhal <ksinghal1997@gmail.com>
2017-10-02 18:27:55 +0530

    Add files via upload

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bootstrap3',
     'accounts',
     'groups',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
